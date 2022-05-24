1fe668b67f06847ad01d71d2a6b2f24530508a54
Davi Moreira da Costa <davimoreira7891@gmail.com>
2022-05-24 09:27:28 -0300

    Terminado

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,4 @@
-import {dobr, somar, temp, media } from './services.js'
+import {dobr, somar, temp, media, array, corprim, cinema, maior, repetir } from './services.js'
 
 import { Router } from 'express'
 const server = Router();
@@ -68,14 +68,13 @@ server.get('/temp', (req, resp) => {
 
     }
     
-
-} )
+})
 
 server.post('/media', (req, resp) => {
     try{
-    const a = Number(req.query.a);
-    const b = Number(req.query.b);
-    const d = Number(req.query.c);
+    const a = Number(req.body.a);
+    const b = Number(req.body.b);
+    const d = Number(req.body.c);
 
     const c = media(a, b, d);
 
@@ -95,6 +94,100 @@ server.post('/media', (req, resp) => {
 
 } )
 
+server.get('/tabuada', (req, resp) => {
+    try{
+    const a = req.query.a;
+
+    const b = array(a)
+
+    resp.send({
+        x: b
+    })
+
+    }catch(err){
+
+        resp.status(404).send({
+            erro:err.message
+
+        })
+    }
+})
+
+
+server.get ('/dia2/corprimaria/:cor', (req,resp) => {
+    try {
+        const a = req.params.cor
+
+        const b = corprim(a)
+
+        resp.send ({
+            primaria:b
+        })
+
+    } catch (err) {
+        resp.status(404).send({
+            error:err.message
+        })
+    }
+
+})
+
+server.post('/dia2/ingressocinema', (req, resp) => {
+    try{
+        const a = req.body.a
+        const b = req.body.b
+        const c = req.body.c
+        const d = req.body.d
+
+        const e = cinema(a, b, c, d)
+
+        resp.send({
+            Total: e
+
+        })
+    }catch(err){
+        resp.status(404).send({
+            erro:err.message
+        })
+    }
+})
+
+
+server.post ('/dia2/maiornumero', (req,resp) => {
+    try {
+        const a = req.body.a
+
+        const b = maior(a)
+
+        resp.send({
+            maior:b
+        })
+    } catch (err) {
+        resp.status (404).send({
+            erro:err.message
+        })
+    }
+})
+
+server.get('/dia2/fraqcaracter/:frase/:letra', (req, resp) => {
+    try{
+        const a = req.params.frase;
+        const b = req.params.letra;
+
+        const c = repetir(a, b)
+
+        resp.send({
+            freq: c
+        })
+
+    }catch(err){
+        resp.status(404).send({
+            erro:err.message
+        })
+
+
+    }
+})
 
 
 export default server;
--- src/services.js
+++ src/services.js
@@ -27,5 +27,63 @@ export function media(n1, n2, n3 ){
     return a;
 }
 
+export function array(n1){
+    let a  = [0,0,0,0,0,0,0,0,0,0,0];
 
+    for (let i = 0; i < 11; i++) {
+        a[i] = i * n1;
+    }
+    return a
+}
 
+export function corprim(cor){
+    let msg = false ;
+    let menor = cor.toLowerCase();
+    
+
+    if(menor == "vermelho" || menor == "amarelo" || menor == "azul")
+        msg = true
+
+        return msg;
+}
+
+export function cinema(inteiras, meias, semana, nacional){
+    let int = 28.50;
+    let met = 14.25;
+    let valortotal = 0;
+
+    if(nacional == "Brasileira"){
+        int = 5;
+        met = 5;
+    }
+
+    else if(semana == "quarta"){
+        int = 14.25;
+        met = 7.12;
+    }
+
+    valortotal = (int * inteiras) + (meias * met);
+
+    return valortotal;
+}
+
+export function maior (n){
+    let total=0
+    for (let i = 0; i < n.length; i++) {
+        if( n[i]>total )
+        total=n[i]
+    }
+return total
+}
+
+export function repetir(frase, letra){
+    let vezes = 0;
+
+    for (let i = 0; i < frase.length; i++) {
+        if(frase[i] == letra)
+            vezes++
+
+    }
+
+    return vezes;
+}
